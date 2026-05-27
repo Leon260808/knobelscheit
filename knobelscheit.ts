0b3836adf4ad4e65f3f3d08359cac378189c1124
Leon Tran <leon_tran@sluz.ch>
2026-05-27 11:44:16 +0200

    implemented getCombinations by using backtracking

--- knobelscheit.ts
+++ knobelscheit.ts
@@ -1,3 +1,23 @@
 export function roll() {
     return Math.floor(Math.random() * 6) + 1;;
 }
+
+export function getCombinations(target: number, availableNumbers: number[]): number[][] {
+    const results: number[][] = [];
+
+    function backtrack(startIndex: number, currentCombination: number[], remainingNumbers: number): void {
+        if (remainingNumbers === 0) {
+            results.push([...currentCombination]);
+            return;
+        }
+        for (let index = startIndex; index < availableNumbers.length; index++) {
+            if (availableNumbers[index] <= remainingNumbers) {
+                currentCombination.push(availableNumbers[index]);
+                backtrack(index + 1, currentCombination, remainingNumbers - availableNumbers[index]);
+                currentCombination.pop();
+            }
+        }
+    }
+    backtrack(0, [], target)
+    return results;
+}
--- knobelscheit_test.ts
+++ knobelscheit_test.ts
@@ -1,15 +1,25 @@
 import { assertEquals } from "@std/assert";
-import { roll } from "./knobelscheit.ts";
+import { roll, getCombinations } from "./knobelscheit.ts";
 
-Deno.test("roll_minOne", function addTest(){
+Deno.test("roll_minOne", function addTest() {
     assertEquals(roll() >= 1, true);
 });
 
-Deno.test("roll_maxSix", function addTest(){
-    const rollResults: number[] = Array.from({length: 100}, roll);
+Deno.test("roll_maxSix", function addTest() {
+    const rollResults: number[] = Array.from({ length: 100 }, roll);
 
     assertEquals(rollResults.every(result => result >= 1 && result <= 6), true);
     assertEquals(new Set(rollResults).size > 1, true);
+});
+
+Deno.test("getCombinations_target7_containsSeven", function addTest() {
+    const combinations = getCombinations(7, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    assertEquals(combinations.some(combination => combination.length === 1 && combination[0] === 7), true)
+});
+
+Deno.test("getCombinations_target7_containsThreeAndFour", function addTest() {
+    const combinations = getCombinations(7, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    assertEquals(combinations.some(combination => combination.includes(3) && combination.includes(4)), true)
 });
 
 
